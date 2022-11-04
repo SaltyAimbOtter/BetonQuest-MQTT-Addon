d23481667090e247b4b703b4986d8f0538cc9b43
saltyaimbotter <saltyaimbotter@gmail.com>
2022-11-04 15:01:40 +0100

    initial commit

---
+++ src/main/java/me/saltyaimbotter/bqmqtt/Bqmqtt.java
@@ -0,0 +1,17 @@
+package me.saltyaimbotter.bqmqtt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Bqmqtt extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
